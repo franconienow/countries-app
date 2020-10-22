ba0e8f6c7bf9d9536bbf5dcccc1d0ab43a4bb5a3
Franco Nienow <fnienow.sn@gmail.com>
2020-10-22 16:47:07 -0300

    Updates

---
+++ precache-manifest.adf8296709530834e33b72688389a44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fe8b2c4767891bc0e0ce8cb2ebc107",
+    "url": "/countries-app/index.html"
+  },
+  {
+    "revision": "b76dcc4064c5f5ae1f2b",
+    "url": "/countries-app/static/css/main.b7949ca1.chunk.css"
+  },
+  {
+    "revision": "7c4a784d67ac60422092",
+    "url": "/countries-app/static/js/2.0292bd81.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/countries-app/static/js/2.0292bd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76dcc4064c5f5ae1f2b",
+    "url": "/countries-app/static/js/main.ac347f53.chunk.js"
+  },
+  {
+    "revision": "57004e892b69719c8568",
+    "url": "/countries-app/static/js/runtime-main.07c6d4b7.js"
+  }
+]);
