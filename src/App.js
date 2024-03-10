2f7097de390079183842016395684c659b4c91f6
marke-franco-nienow <franco@marke.com.br>
2024-03-10 01:35:04 -0300

    Update na API e refactoring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CountrieCard from "./components/CountrieCard/CountrieCard";
-import Input from './components/Input/Input'
-import Filter from './components/Filter/Filter'
-import DetailsPage from './components/DetailsPage/DetailsPage'
-import GlobalStyle from './styles/global';
-import Header from './components/Header/Header';
+import Input from "./components/Input/Input";
+import Filter from "./components/Filter/Filter";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+import GlobalStyle from "./styles/global";
+import Header from "./components/Header/Header";
 
 function App() {
-
   const [countryList, setCountryList] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [regionFilter, setRegionFilter] = useState("All");
@@ -21,24 +16,24 @@ function App() {
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
 
-  const fetchData = async () =>{
-    const res = await fetch("https://restcountries.eu/rest/v2/");
-    const data = await res.json()
-    const cL = data.map(c => {
-      return{
-        alpha3Code: c.alpha3Code,
-        name: c.name,
+  const fetchData = async () => {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const data = await res.json();
+    const cL = data.map((c) => {
+      return {
+        alpha3Code: c.cca2,
+        name: c.name.common,
         population: c.population,
         region: c.region,
-        capital: c.capital,
-        flag: c.flag
-      }
-    })
-    
+        capital: c.capital ? c.capital[0] : '',
+        flag: c.flags.svg,
+      };
+    });
+
     setCountryList(cL);
-  }
+  };
 
   const updataSearchInput = (e) => {
     setSearchInput(e.target.value);
@@ -49,65 +44,68 @@ function App() {
   };
 
   const filteredList = () => {
-    if(regionFilter === "All"){
-      
+    if (regionFilter === "All") {
       return countryList;
-    }else{
-      return countryList.filter(({region}) => {return region === regionFilter})
+    } else {
+      return countryList.filter(({ region }) => {
+        return region === regionFilter;
+      });
     }
   };
 
   const dynamicSearch = () => {
-    return filteredList().filter(({name}) => {
-      return name.toLowerCase().includes(searchInput.toLowerCase())})
+    return filteredList().filter(({ name }) => {
+      return name.toLowerCase().includes(searchInput.toLowerCase());
+    });
   };
 
   const createElements = () => {
-    return(
-      dynamicSearch().map(country => (
-        <CountrieCard theme={theme}
-        key={country.alpha3Code} 
-        code = {country.alpha3Code} 
+    return dynamicSearch().map((country) => (
+      <CountrieCard
+        theme={theme}
+        key={country.alpha3Code}
+        code={country.alpha3Code}
         name={country.name}
         population={country.population}
         region={country.region}
         capital={country.capital}
         flag={country.flag}
-        />
-      ))
-    )
-  }
+      />
+    ));
+  };
 
   const handleTheme = () => {
-    if(theme === 'dark'){
-      setTheme('ligth')
-    }else{
-      setTheme('dark')
+    if (theme === "dark") {
+      setTheme("ligth");
+    } else {
+      setTheme("dark");
     }
-  }
+  };
 
-  
   return (
     <Router>
-      <GlobalStyle theme={theme}/>
-      <Header handleTheme={handleTheme} theme={theme}/>
+      <GlobalStyle theme={theme} />
+      <Header handleTheme={handleTheme} theme={theme} />
       <div className="main-container">
         <Switch>
-          <Route path="/details/:id"><DetailsPage theme={theme} /></Route>
+          <Route path="/details/:id">
+            <DetailsPage theme={theme} />
+          </Route>
           <Route path="/">
             <div className="inputContainer">
-              <Input theme={theme} type="text" value={searchInput} onChange={updataSearchInput}/>
-              <Filter theme={theme} onClick={updateFiterInput}/>
+              <Input
+                theme={theme}
+                type="text"
+                value={searchInput}
+                onChange={updataSearchInput}
+              />
+              <Filter theme={theme} onClick={updateFiterInput} />
             </div>
-            <div className='cardsContainer'>
-              {createElements()}
-            </div>
+            <div className="cardsContainer">{createElements()}</div>
           </Route>
-
         </Switch>
       </div>
     </Router>
-    
   );
 }
 
--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -1,90 +1,149 @@
-import React, { useEffect, useState } from 'react';
-import { Link , useParams } from "react-router-dom";
-import { InfoContainer, Flag, InfoDiv, CName, InfoCol, NeighborBtn, BackBtn, BackIcon } from "./DetailsPage.style";
-import { Info } from '../CountrieCard/CountrieCard.style';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import {
+  InfoContainer,
+  Flag,
+  InfoDiv,
+  CName,
+  InfoCol,
+  NeighborBtn,
+  BackBtn,
+  BackIcon,
+} from "./DetailsPage.style";
+import { Info } from "../CountrieCard/CountrieCard.style";
 
-function DetailsPage(props){
-
+function DetailsPage(props) {
   let { id } = useParams();
-  
+
   const [country, setCountry] = useState({});
-  const [neighbors, setNeighbors] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
-      const data = await res.json()
-      setCountry(data)
-    };
-    fetchData();
-  }, [id])
-
-  useEffect(() => {
-    const fetchBordersData = async () => {
-      if(country.borders !== undefined && country.borders.length > 0){
-        const res = await fetch(`https://restcountries.eu/rest/v2/alpha?codes=${country.borders.join(';')}`);
-        const data = await res.json()
-        setNeighbors(data.map(country => {return {name: country.name, id: country.alpha3Code}}));
+      const res = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
+      const data = await res.json();
+      console.log(data[0]);
+      if (data[0]) {
+        setCountry({
+          id: data[0].cca2,
+          name: data[0].name.common,
+          flag: data[0].flags.svg,
+          nativeNames: data[0].name.nativeName,
+          population: data[0].population,
+          region: data[0].region,
+          subregion: data[0].subregion,
+          capital: data[0].capital ? data[0].capital[0] : "",
+          topLevelDomain: data[0].tld[0],
+          currencies: data[0].currencies,
+          languages: data[0].languages,
+          borders: data[0].borders,
+        });
       }
     };
-    fetchBordersData()
-  }, [country])
+    fetchData();
+  }, [id]);
 
-  const renderCurrencies = (curr) => {
-    if(curr !== undefined){
-      return curr.map(c => c.name).join(", ");
+  const renderCurrencies = (currencies) => {
+    if (currencies !== undefined) {
+      let currenciesArr = [];
+      for (const prop in currencies) {
+        currenciesArr.push(currencies[prop].name);
+      }
+      return currenciesArr.join(", ");
     }
   };
 
-  const renderLangs = (lang) => {
-    if(lang !== undefined){
-      return lang.map(l => l.name).join(", ");
+  const renderLangs = (langs) => {
+    if (langs !== undefined) {
+      let langsArr = [];
+      for (const prop in langs) {
+        langsArr.push(langs[prop]);
+      }
+      return langsArr.join(", ");
     }
   };
 
-  const renderBorders = countryList => {
-    if(countryList !== undefined){
-      return countryList.map((c) => {
+  const renderNativeNames = (nativeNames) => {
+    if (nativeNames !== undefined) {
+      let nativeNamesArr = [];
+      for (const prop in nativeNames) {
+        nativeNamesArr.push(nativeNames[prop].common);
+      }
+      return nativeNamesArr.join(", ");
+    }
+  };
+
+  const renderBorders = (countryList) => {
+    if (countryList !== undefined) {
+      console.log(countryList);
+      return countryList.map((countryName) => {
         return (
-        <Link key={c.id} style={{ textDecoration: 'none', color: 'white' }} to={`/details/${c.id}`}>
-          <NeighborBtn theme={props.theme}>{c.name.length < 12? c.name: c.id}</NeighborBtn>
-        </Link>)       
-      }) 
+          <Link
+            key={countryName}
+            style={{ textDecoration: "none", color: "white" }}
+            to={`/details/${countryName}`}
+          >
+            <NeighborBtn theme={props.theme}>{countryName}</NeighborBtn>
+          </Link>
+        );
+      });
     }
   };
 
-  return(
+  return (
     <div>
-      <Link 
-        style={{ textDecoration: 'none', color: 'white' }} to={`/`}><BackBtn theme={props.theme}><BackIcon theme={props.theme}/>Back</BackBtn>
+      <Link style={{ textDecoration: "none", color: "white" }} to={`/`}>
+        <BackBtn theme={props.theme}>
+          <BackIcon theme={props.theme} />
+          Back
+        </BackBtn>
       </Link>
       <InfoContainer key={country.id}>
-        <Flag src={country.flag} alt={country.name}/>
-          <InfoDiv theme={props.theme}>
-            <CName>{country.name}</CName>
-            <InfoCol display={'inline'} columnSpace={'1'}>
-              <p><Info>Native Name: </Info>{country.nativeName}</p>
-              <p><Info>Population: </Info>{Intl.NumberFormat('pt-BR').format(country.population)}</p>
-              <p><Info>Region: </Info>{country.region}</p>
-              <p><Info>Sub Region: </Info>{country.subregion}</p>
-              <p><Info>Capital: </Info>{country.capital}</p>
-            </InfoCol>
+        <Flag src={country.flag} alt={country.name} />
+        <InfoDiv theme={props.theme}>
+          <CName>{country.name}</CName>
+          <InfoCol display={"inline"} columnSpace={"1"}>
+            <p>
+              <Info>Native Name: </Info>
+              {renderNativeNames(country.nativeNames)}
+            </p>
+            <p>
+              <Info>Population: </Info>
+              {Intl.NumberFormat("pt-BR").format(country.population)}
+            </p>
+            <p>
+              <Info>Region: </Info>
+              {country.region}
+            </p>
+            <p>
+              <Info>Sub Region: </Info>
+              {country.subregion}
+            </p>
+            <p>
+              <Info>Capital: </Info>
+              {country.capital}
+            </p>
+          </InfoCol>
 
-            <InfoCol display={'inline'} columnSpace={'2'}>
-              <p><Info>Top Level Domain: </Info>{country.topLevelDomain}</p>
-              <p><Info>Currencies: </Info>{renderCurrencies(country.currencies)}</p>
-              <p><Info>Languages: </Info>{renderLangs(country.languages)}</p>
-            </InfoCol>
+          <InfoCol display={"inline"} columnSpace={"2"}>
+            <p>
+              <Info>Top Level Domain: </Info>
+              {country.topLevelDomain}
+            </p>
+            <p>
+              <Info>Currencies: </Info>
+              {renderCurrencies(country.currencies)}
+            </p>
+            <p><Info>Languages: </Info>{renderLangs(country.languages)}</p>
+          </InfoCol>
 
-            <InfoCol display={'inline'} columnSpace={'1 / 3'}>
-              <Info>Borders: </Info>
-              {renderBorders(neighbors)}
-            </InfoCol>
-          </InfoDiv>
+          <InfoCol display={"inline"} columnSpace={"1 / 3"}>
+            <Info>Borders: </Info>
+            {renderBorders(country.borders)}
+          </InfoCol>
+        </InfoDiv>
       </InfoContainer>
     </div>
-    
   );
-};
+}
 
 export default DetailsPage;
