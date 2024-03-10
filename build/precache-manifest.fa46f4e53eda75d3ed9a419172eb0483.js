a6ca3ad890e6b18d063aef0a5ec41223bbab6fda
franconienow <fnienow.sn@gmail.com>
2024-03-10 01:44:42 -0300

    build na branch master

---
+++ build/precache-manifest.fa46f4e53eda75d3ed9a419172eb0483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fb4e7c00f86a0fefe34868893c5128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c629b3560022aab889f",
+    "url": "/static/css/main.fd272e61.chunk.css"
+  },
+  {
+    "revision": "791f927fed357427269b",
+    "url": "/static/js/2.9e654e9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9e654e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c629b3560022aab889f",
+    "url": "/static/js/main.ccf6c275.chunk.js"
+  },
+  {
+    "revision": "28fc76dfc3e9feba5311",
+    "url": "/static/js/runtime-main.4677a4e5.js"
+  }
+]);
